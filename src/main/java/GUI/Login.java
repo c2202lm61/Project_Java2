85a76f04b65adcca198c945354ff0c74c2474444
c2202lm <c2202lm.pmquan@aptech.vn>
2023-07-13 23:35:20 +0700

    handle Layout

--- src/main/java/GUI/Login.java
+++ src/main/java/GUI/Login.java
@@ -1,6 +1,8 @@
 package GUI;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class Login extends JDialog{
     public Login(){
@@ -10,6 +12,23 @@ public class Login extends JDialog{
         setLocationRelativeTo(null);
         setDefaultCloseOperation(DISPOSE_ON_CLOSE);
         setVisible(true);
+        btnLogin.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                if (textField1.getText().equals("admin")&&passwordField1.getText().equals("1111")) {
+                    new MainGUI();
+                    dispose();
+                }
+                else
+                    JOptionPane.showMessageDialog(btnLogin,"Thông tin tài khoản hoặc mật khẩu không chính xác!");
+            }
+        });
+        registerButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                new Register();
+            }
+        });
     }
     private JTextField textField1;
     private JPasswordField passwordField1;
--- src/main/java/com/score/management/Main.java
+++ src/main/java/com/score/management/Main.java
@@ -5,10 +5,6 @@ import GUI.*;
 
 public class Main {
     public static void main(String[] args) {
-        JDBCDriver connectMysql = new JDBCDriver();
-        MainGUI mainGUI  = new MainGUI();
-        Register register = new Register();
         Login login =new Login();
-
     }
 }
