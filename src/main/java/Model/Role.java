fb751a947295e8baf32d5d4354b2e0c5d984da1a
Dương <duongngo533@gmail.com>
2023-07-17 15:32:53 +0700

    hihi dương up model

--- src/main/java/Model/Role.java
+++ src/main/java/Model/Role.java
@@ -1,4 +1,9 @@
 package Model;
 
 public class Role {
+    public int id;
+    public String name;
+
+    public void setId(int id){this.id = id;}
+    public void setName(String name){this.name = name;}
 }
--- src/main/java/Model/SubjectStudent.java
+++ src/main/java/Model/SubjectStudent.java
@@ -1,4 +1,11 @@
 package Model;
 
 public class SubjectStudent {
+    public int Subject_Student_id;
+    public int student_id;
+    public int Subject_code;
+
+    public void setSubject_code(int Subject_code){this.Subject_code = Subject_code;}
+    public void setStudent_id(int student_id){this.student_id = student_id;}
+    public void setSubject_Student_id(int Subject_Student_id){this.Subject_Student_id = Subject_Student_id;}
 }
