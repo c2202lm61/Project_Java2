15920b6563ac012121bd60afbf49b166d2821b58
Dương <duongngo533@gmail.com>
2023-07-17 15:51:36 +0700

    duongupcode

--- src/main/java/DAO/Access/InstructorRoleHandle.java
+++ src/main/java/DAO/Access/InstructorRoleHandle.java
@@ -1,8 +1,12 @@
 package DAO.Access;
 
+import DAO.JDBCDriver;
+import Model.Instructor;
 import Model.InstructorRole;
 
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class InstructorRoleHandle  extends AbsSQLAccess<InstructorRole> {
@@ -13,7 +17,17 @@ public class InstructorRoleHandle  extends AbsSQLAccess<InstructorRole> {
 
     @Override
     public List<InstructorRole> SELECT(String sql) throws SQLException {
-        return null;
+        List<InstructorRole> a = new ArrayList<>();
+        final ResultSet resultSet = JDBCDriver.ExecQuery(sql);
+        while (resultSet.next()){
+            InstructorRole b = new InstructorRole();
+            b.setID_NUMBER(resultSet.getInt("ID_NUMBER"));
+            b.setRole_id(resultSet.getInt("Role_id"));
+            b.setTclass_id(resultSet.getInt("Tclass_id"));
+            a.add(b);
+        }
+return a;
+
     }
 
     @Override
