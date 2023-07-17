0420edb93fa11c4d9214a26be536d32cf3d99040
Tung Dinh <tungdinh21092003@gmail.com>
2023-07-17 15:54:43 +0700

    tung type score

--- src/main/java/Model/TypeScore.java
+++ src/main/java/Model/TypeScore.java
@@ -1,4 +1,19 @@
 package Model;
 
 public class TypeScore {
+    public int id;
+    public String Name;
+    public void setID(int ID){
+        this.id= id;
+    }
+    public int getID(){
+        return this.id;
+    }
+    public void setName(String Name){
+        this.Name = Name;
+    }
+    public String getName(){
+        return this.Name;
+    }
+
 }
