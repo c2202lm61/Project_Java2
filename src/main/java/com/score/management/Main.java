de2b1bbdab7768df64c6adfb7e853aeafb3ab231
c2202lm <c2202lm.pmquan@aptech.vn>
2023-07-19 20:33:00 +0700

    add insert

--- src/main/java/com/score/management/Main.java
+++ src/main/java/com/score/management/Main.java
@@ -1,11 +1,13 @@
 package com.score.management;
 import DAO.Access.GrantHandle;
 import DAO.Access.InstructorHandle;
+import DAO.Access.TypeScoreHandle;
 import DAO.JDBCDriver;
 import GUI.*;
 import Model.Block;
 import Model.Instructor;
 import Model.Student;
+import Model.TypeScore;
 
 import javax.swing.*;
 import java.sql.ResultSet;
@@ -15,12 +17,7 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        Block a = new Block();
-        a.setID(4);
-        a.setName("Lop2");
-       // new GrantHandle().INSERT(a);
-    //    new GrantHandle().DELETE(6);
-        new GrantHandle().UPDATE(a);
+
         Instructor instructor = new Instructor();
         instructor.setName("Nak");
         instructor.setPassword("!23");
