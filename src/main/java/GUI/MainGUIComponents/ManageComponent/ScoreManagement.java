623057c2973e7bd88d8865a674e606b19096fd7f
c2202lm <c2202lm.pmquan@aptech.vn>
2023-07-26 14:22:39 +0700

    add update suject code and student name in viewscocre management

--- src/main/java/GUI/MainGUIComponents/ManageComponent/ScoreManagement.java
+++ src/main/java/GUI/MainGUIComponents/ManageComponent/ScoreManagement.java
@@ -1,48 +1,54 @@
 package GUI.MainGUIComponents.ManageComponent;
 
-import DAO.Access.ScoreStudentHandle;
-import DAO.Access.SubjectStudentHandle;
-import Model.Score;
-import Model.SubjectStudent;
+import DAO.Access.StudentHandle;
+import DAO.Access.SubjectHandle;
+import Model.Student;
+import Model.Subject;
 
 import javax.swing.*;
 import javax.swing.border.LineBorder;
 import javax.swing.table.DefaultTableModel;
 import java.awt.*;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 public class ScoreManagement extends JInternalFrame {
+    private List<Subject> subjectList = new ArrayList<>();
+    private List<Student> studentList = new ArrayList<>();
     private JPanel scoreManageComponent;
-    private JButton button1;
-    private JButton button2;
-    private JButton button3;
-    private JButton button4;
-    private JButton button5;
-    private JCheckBox checkBox1;
-    private JCheckBox checkBox2;
-    private JComboBox comboBox1;
     private JTable table1;
-    private JTextField MaHocSinh;
-    private JTextField MaMonHoc;
-    private JTextField Diem15;
-    private JTextField Diem45;
-    private JTextField DiemThiGiuaKi1;
-    private JTextField DiemThiCuoiKi1;
-    private JTextField DiemThiGiuaKi2;
-    private JTextField DiemThiCuoiKi2;
+    private JTextField scmStudentName;
+    private JTextField scmSubjectCode;
+    private JTextField scmStudentCode;
+    private JButton chọnẢnhButton;
+    private JButton tảiLạiButton;
+    private JButton xóaButton;
+    private JButton sửaButton;
+    private JButton thêmButton;
+    private JComboBox scmSubjectName;
 
+    public void updateData() throws SQLException {
 
+        //get subject data and add commbocox
+        try {
+            subjectList = new SubjectHandle().SELECT("SELECT * FROM `subject`");
+        }catch (SQLException e){
+            e.printStackTrace();
+        }
+        Iterator<Subject> subjectIterator = subjectList.iterator();
+        while (subjectIterator.hasNext()){
+            Subject item = subjectIterator.next();
+            scmSubjectName.addItem(item.getName());
+        }
 
-    public static void main(String[] args) {
-        JFrame frame = new JFrame("ScoreManagement");
-        frame.setContentPane(new ScoreManagement().scoreManageComponent);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.pack();
-        frame.setVisible(true);
     }
-
-    public ScoreManagement(){
+    public ScoreManagement() throws SQLException {
 
 
         //------------------------------------------------------
@@ -58,11 +64,50 @@ public class ScoreManagement extends JInternalFrame {
         modelScoreManage.addColumn("Điểm thi cuối kì 2");
         //-----------------------------------------------
         table1.setModel(modelScoreManage);
+        updateData();
 
         //-----------------------------------------------------------------
         setBorder(new LineBorder(new Color(168, 167, 167, 226),1));
         setContentPane(scoreManageComponent);
         setVisible(true);
-        //----------------------------------------------------------------------
+        //show studentName when ID true
+
+        //update subject id when change value
+        scmSubjectName.addItemListener(new ItemListener() {
+            @Override
+            public void itemStateChanged(ItemEvent e) {
+                if (e.getStateChange() == ItemEvent.SELECTED) {
+                    // Lấy giá trị của scmSubjectCode khi giá trị của JComboBox thay đổi
+                    String selectedValue = (String) scmSubjectName.getSelectedItem();
+                    System.out.println("Selected scmSubjectName: " + selectedValue);
+                    Iterator<Subject> list = subjectList.iterator();
+                    while (list.hasNext()){
+                        Subject item =  list.next();
+                        if (item.getName().equals(scmSubjectName.getSelectedItem())){
+                            System.out.println("Subject selectID: "+item.getID());
+                            scmSubjectCode.setText(String.valueOf(item.getID()));
+                        }
+                    }
+                }
+            }
+        });
+        scmStudentCode.addKeyListener(new KeyAdapter() {
+            @Override
+            public void keyReleased(KeyEvent e) {
+                System.out.println(scmStudentCode.getText());
+                try {
+                    studentList = new StudentHandle().SELECT("SELECT * FROM `student`");
+                }catch (SQLException exception){
+                    exception.printStackTrace();
+                }
+                Iterator<Student> studentIterator = studentList.iterator();
+                while (studentIterator.hasNext()){
+                    Student item = studentIterator.next();
+                    if (item.getID() == Integer.parseInt(scmStudentCode.getText())){
+                        scmStudentName.setText(item.getName());
+                    }
+                }
+            }
+        });
     }
 }
