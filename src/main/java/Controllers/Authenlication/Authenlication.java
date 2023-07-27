56854033536e8dc75cef72790f3a0ed0c32398ea
c2202lm <c2202lm.pmquan@aptech.vn>
2023-07-27 21:30:47 +0700

    add login, register

--- src/main/java/Controllers/Authenlication/Authenlication.java
+++ src/main/java/Controllers/Authenlication/Authenlication.java
@@ -1,6 +1,7 @@
 package Controllers.Authenlication;
 
 import Controllers.Validation;
+import DAO.Access.InstructorHandle;
 import DAO.JDBCDriver;
 import GUI.MainGUI;
 import Model.Instructor;
@@ -18,16 +19,46 @@ public class Authenlication {
             System.out.println("Password is not valid"); return;
         }
         ResultSet resultSet =JDBCDriver.ExecQuery("SELECT * FROM instructor WHERE Email = "+"'"+email+"'");
-        if(!resultSet.next()){
-            System.out.println("Email is not exist");  return;
-        }else {
-            System.out.println("Email has exist");
-        }
 
+        while (resultSet.next()){
+            if(email.equals(resultSet.getString("Email")) && password.equals(resultSet.getString("Password"))){
+                new MainGUI();
+            }else {
+                System.out.println("Thông tin tài khoản hoặc mât khẩu không chính xác");
+            }
+            return;
+        }
+        System.out.println("Email không tồn tại");
         //do something
     }
-    public static void Register(String email,String password, String phone){
-        //do something
+    public static void Register(String email,String password, String fullname){
+        if (!Validation.isFullName(fullname)){
+            System.out.println("Your fullname is not valid");
+            return;
+        }
+        if(!Validation.isEmail(email)){
+            System.out.println("Email is not valid");
+            return;
+        }
+        if(!Validation.isStrongPassword(password)){
+            System.out.println("Password is not valid");
+            return;
+        }
+        try {
+            if(Validation.hasEmail(email)){System.out.println("Email is has exist"); return;}
+        }catch (SQLException e) {
+            e.printStackTrace();
+        }
+        try {
+          Boolean result =  JDBCDriver.SetQuery("INSERT INTO `instructor`(`name`, `password`, `Email`,`Phone`) VALUES ('"+fullname+"','"+password+"','"+email+"','01234567')");
+          if(result){
+              System.out.println("Thêm dữ liệu thành công");
+          }else {
+              System.out.println("Thêm dữ liệu không thành công");
+          }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
     public static void ForgotPassword(){
         
---
+++ src/main/java/GUI/MainGUIComponents/Alert.java
@@ -0,0 +1,8 @@
+package GUI.MainGUIComponents;
+
+import javax.swing.*;
+
+public class Alert {
+    private JPanel panel1;
+    private JButton OKButton;
+}
