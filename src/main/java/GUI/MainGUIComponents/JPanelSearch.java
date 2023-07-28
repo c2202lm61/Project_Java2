6cd2a24c9f95553c8e228f7f417df7fbe6bc9f10
c2202lm <c2202lm.pmquan@aptech.vn>
2023-07-28 20:53:25 +0700

    change form

--- src/main/java/GUI/MainGUIComponents/JPanelSearch.java
+++ src/main/java/GUI/MainGUIComponents/JPanelSearch.java
@@ -1,110 +1,27 @@
 package GUI.MainGUIComponents;
 
 import javax.swing.*;
-import javax.swing.border.EmptyBorder;
-import javax.swing.table.DefaultTableModel;
-import javax.swing.table.TableColumn;
 import java.awt.*;
 
-public class JPanelSearch extends JPanel {
+public class JPanelSearch extends JPanel{
     public JPanelSearch(){
-        setLayout(new BorderLayout());
-        add(new HeaderPanel(),BorderLayout.NORTH);
-        add(new ContentPanel(),BorderLayout.CENTER);
+        setLayout(new GridLayout(1,1));
+        add(panel1);
     }
-}
-class HeaderPanel extends JPanel{
-    public HeaderPanel(){
-        setBackground(new Color(32,178,170));
-        setLayout(new BorderLayout());
-        setLayout(new BorderLayout());
-        setBorder(new EmptyBorder(10,20,10,20));
-        JPanel topPanel = new JPanel();
-        topPanel.setBackground(new Color(32,178,170));
-        JPanel bottomPanel = new JPanel(new FlowLayout(FlowLayout.CENTER));
-        bottomPanel.setBackground(new Color(32,178,170));
-        JLabel label = new JLabel("Tìm kiếm");
-        topPanel.add(label);
-        JComboBox JChooseSeach = new JComboBox();
-        JChooseSeach.addItem("Học sinh");
-        JChooseSeach.addItem("Giáo viên");
-        JChooseSeach.addItem("Môn học");
-        JChooseSeach.addItem("Lớp học");
-        JChooseSeach.addItem("Khối học");
-        bottomPanel.add(JChooseSeach);
-        bottomPanel.add(new JTextField(15));
-        bottomPanel.add(new JButton("Tìm"));
-        add(topPanel,BorderLayout.NORTH);
-        add(bottomPanel,BorderLayout.SOUTH);
-    }
-}
-class ContentPanel extends JPanel{
-    public ContentPanel(){
-        setLayout(new GridLayout());
-        add(new searchTabblepane());
-    }
-}
-class searchTabblepane extends JTabbedPane{
-    public searchTabblepane(){
-        addTab("Sinh Viên",new JScrollPane(new JStudentTable()));
-        addTab("Giáo viên",new JScrollPane(new JTeacherTable()));
-        addTab("Khối",new JScrollPane(new JBlockTable()));
-        addTab("Lớp",new JScrollPane(new JClassTable()));
-        addTab("Môn",new JScrollPane(new JSubjectTable()));
-    }
-}
-class JStudentTable extends JTable{
-    public JStudentTable(){
-        DefaultTableModel  model = new DefaultTableModel();
-        model.addColumn("id");
-        model.addColumn("Họ tên");
-        model.addColumn("Ngày  sinh");
-        model.addColumn("Địa chỉ");
-        model.addColumn("Giới tính");
-        model.addColumn("Số điện thoại");
-        model.addRow(new Object[]{1,"do nam tram","1/2/2002","noi nao do","3d","0123456789"});
-        setModel(model);
-    }
-}
-class JTeacherTable extends JTable{
-    public JTeacherTable(){
-        DefaultTableModel  model = new DefaultTableModel();
-        model.addColumn("id");
-        model.addColumn("Họ tên");
-        model.addColumn("Ngày  sinh");
-        model.addColumn("Địa chỉ");
-        model.addColumn("Giới tính");
-        model.addColumn("Số điện thoại");
-        model.addRow(new Object[]{1,"giao vien","1/2/2002","noi nao do","3d","0123456789"});
-        setModel(model);
-    }
-}
-class JBlockTable extends JTable{
-    public JBlockTable(){
-        DefaultTableModel  model = new DefaultTableModel();
-        model.addColumn("id");
-        model.addColumn("Tên khối");
-        model.addRow(new Object[]{1,"A"});
-        setModel(model);
-    }
-}
-class JClassTable extends JTable{
-    public JClassTable(){
-        DefaultTableModel  model = new DefaultTableModel();
-        model.addColumn("id");
-        model.addColumn("Tên Lớp");
-        model.addColumn("id khối");
-        model.addColumn("TeacherId");
-        model.addRow(new Object[]{1,"1a",1,12});
-        setModel(model);
-    }
-}
-class JSubjectTable extends JTable{
-    public JSubjectTable(){
-        DefaultTableModel  model = new DefaultTableModel();
-        model.addColumn("id");
-        model.addColumn("Tên môn hoc");
-        model.addRow(new Object[]{1,"Math"});
-        setModel(model);
-    }
+    private JPanel panel1;
+    private JTabbedPane tabbedPane1;
+    private JTable table1;
+    private JTextField textField1;
+    private JButton tìmButton;
+    private JTable table2;
+    private JPasswordField passwordField1;
+    private JButton tìmButton1;
+    private JTextField textField2;
+    private JButton tìmButton2;
+    private JTable table3;
+    private JTable table4;
+    private JTextField textField3;
+    private JButton tìmButton4;
+    private JButton tìmButton3;
+    private JTextField textField4;
 }
--- src/main/java/com/score/management/Main.java
+++ src/main/java/com/score/management/Main.java
@@ -2,17 +2,19 @@ package com.score.management;
 
 import Controllers.Authenlication.Authenlication;
 import Controllers.Validation;
+import DAO.Access.InstructorHandle;
 import GUI.Login;
 import GUI.MainGUI;
 import GUI.Register;
+import Model.Instructor;
 
 import java.sql.SQLException;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
 //        new Login();
-//        new Register();
-//        new MainGUI();
-
+      //  new Register();
+        new MainGUI();
     }
 }
