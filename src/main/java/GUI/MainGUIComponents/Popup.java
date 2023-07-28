efebf921ecb234426e096de980714634aca8ce8a
Dương <duongngo533@gmail.com>
2023-07-28 20:37:47 +0700

    hi

---
+++ src/main/java/GUI/MainGUIComponents/Popup.java
@@ -0,0 +1,11 @@
+package GUI.MainGUIComponents;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.sql.SQLException;
+public class Popup {
+    private JButton OKButton;
+
+
+}
--- src/main/java/com/score/management/Main.java
+++ src/main/java/com/score/management/Main.java
@@ -2,7 +2,12 @@ package com.score.management;
 import Controllers.Authenlication.Authenlication;
 import DAO.Access.*;
 import GUI.*;
+import GUI.MainGUIComponents.Popup;
 import Model.*;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import java.sql.SQLException;
 import java.time.LocalDate;
@@ -12,8 +17,13 @@ import java.time.format.DateTimeFormatter;
 
 public class Main {
     public static void main(String[] args) {
+
        new Login();
+
 
 //        new MainGUI();
     }
+
+
 }
+
