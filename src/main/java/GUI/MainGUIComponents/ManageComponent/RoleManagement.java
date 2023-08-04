d18632591bb653d2518bdd2ae59598485b6dcfe4
phamquan <c2202lm.pmquan@aptech.vn>
2023-08-05 03:51:06 +0700

    add  role

---
+++ src/main/java/GUI/MainGUIComponents/ManageComponent/RoleManagement.java
@@ -0,0 +1,12 @@
+package GUI.MainGUIComponents.ManageComponent;
+
+import javax.swing.*;
+
+public class RoleManagement extends JInternalFrame {
+    public RoleManagement(){
+        setContentPane(mainPanel);
+        setVisible(true);
+    }
+    private JPanel mainPanel;
+    private JButton button1;
+}
